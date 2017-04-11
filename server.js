25a2a4cc0d440aae2ab79121865b58353354a0f9
Zach Kessler <zjkessler@gmail.com>
2017-04-11 12:01:20 -0600

    server switch between URI and local

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+'use strict';
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 9000;
-var mongoURI = 'mongodb: heroku_6x5bn9zt: 6 mmd0alhtfnhv0ccvmv3qs7agc @ds159050.mlab.com: 59050 / heroku_6x5bn9zt'
+var env = process.env.NODE_ENV || 'development'
+var mongoURI = 'mongodb:heroku_6x5bn9zt: 6 mmd0alhtfnhv0ccvmv3qs7agc @ds159050.mlab.com: 59050 / heroku_6x5bn9zt';
 
 app.use(bodyParser.json());
 
@@ -15,10 +17,14 @@ app.use(express.static(path.join('./public/')));
 app.use('/upgrades', require('./backend/routes/upgrade-routes'));
 
 //DB connection
-mongoose.connect(mongoURI || 'mongodb://localhost/4runner', function () {
-	'use strict';
-	console.log('connected to database...');
-});
+if (env === development) {
+	mongoose.connect('mongodb://localhost/4runner', function () {
+
+		console.log('connected to database...');
+	});
+} else {
+	mongoose.connect(mongoURI);
+}
 
 
 app.listen(port, function () {
